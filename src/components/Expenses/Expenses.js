d5cf681812c3b503aa6af7815629379f657e498d
fs-projects <fs.projectsuk@gmail.com>
2021-06-15 13:09:53 +0530

    Revert "Add: Chart to display expenses of each year with all the months for that year filled as much as the expenses done for that month"
    This reverts commit bafd6a244c09b51b64f9a679a1a6cae4eeedf13d.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
-import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -24,7 +23,6 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
