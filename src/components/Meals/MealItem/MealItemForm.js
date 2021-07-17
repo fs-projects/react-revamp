7420c061b3934bfd23e14967c90d38e9f610e183
fs-projects <fs.projectsuk@gmail.com>
2021-07-17 21:32:16 +0530

    Add: Context subscription to show cart count-Reducer setup to handle add to cart-grab value from mealitem form input

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,21 +1,39 @@
-import Input from '../../UI/Input';
-import classes from './MealItemForm.module.css';
+import { useRef, useState } from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [formIsValid, setFormIsValid] = useState(true);
+  const inputAmountRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = +inputAmountRef.current.value;
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmount < 1 ||
+      enteredAmount > 5
+    ) {
+      setFormIsValid(false);
+      return;
+    }
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
-        label='Amount'
+        ref={inputAmountRef}
+        label="Amount"
         input={{
-          id: 'amount_' + props.id,
-          type: 'number',
-          min: '1',
-          max: '5',
-          step: '1',
-          defaultValue: '1',
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
         }}
       />
       <button>+ Add</button>
+      {!formIsValid ? <p>Provided inputs are not correct! Try again</p> : null}
     </form>
   );
 };
